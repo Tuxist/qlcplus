a9be5a51a8984feae95462cac7ddbeb65c413bbd
Jan Koester <jan.koester@gmx.net>
2020-03-22 15:27:01 +0100

    fixed allocating memory

--- engine/src/script.cpp
+++ engine/src/script.cpp
@@ -47,11 +47,11 @@ void *Script::operator new(size_t size)
 {
     switch(Script::ScriptVersion){
         case 4:{
-            size=sizeof(ScriptV4);
+            size+=sizeof(ScriptV4);
             break;
         };
         case 3:{
-            size=sizeof(ScriptV3);
+            size+=sizeof(ScriptV3);
             break;
         };
         default:{
@@ -65,11 +65,11 @@ void *Script::operator new(size_t size,void *ptr)
 {
    switch(Script::ScriptVersion){
         case 4:{
-            size=sizeof(ScriptV4);
+            size+=sizeof(ScriptV4);
             break;
         };
         case 3:{
-            size=sizeof(ScriptV3);
+            size+=sizeof(ScriptV3);
             break;
         };
         default:{
